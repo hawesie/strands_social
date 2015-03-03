880f7f0cd14eebcaf8f4fcc766109430113ce3d4
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2015-03-03 21:54:35 +0000

    Basic tweet page displaying. No interaction yet.

--- tweet_me_screen/scripts/tweet_me_screen.py
+++ tweet_me_screen/scripts/tweet_me_screen.py
@@ -1,15 +1,23 @@
 #! /usr/bin/env python
 
 import rospy
+import roslib
+import os
 
-# import strands_webserver.page_utils
-# import strands_webserver.client_utils
+import strands_webserver.client_utils
 # import std_srvs.srv 
 
 if __name__ == '__main__':
-	rospy.init_node("tweet_me_screen")
+    rospy.init_node("tweet_me_screen")
 
-	# The display to publish on, defaulting to all displays
-	display_no = rospy.get_param("~display", 0)	
+    # The display to publish on, defaulting to all displays
+    display_no = rospy.get_param("~display", 0) 
 
-	rospy.spin()
+    # set http root to be this package
+    http_root = os.path.join(roslib.packages.get_pkg_dir('tweet_me_screen'), 'data')
+    strands_webserver.client_utils.set_http_root(http_root)
+
+    # start with a basic page pretending things are going normally
+    strands_webserver.client_utils.display_relative_page(display_no, 'tweet_me.html')
+
+    rospy.spin()
