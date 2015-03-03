15a94325c7f5faf1b30e6eb691314c72790e5d86
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2015-03-03 23:15:41 +0000

    Page is now generated via templates so easiler to use by external code.

---
+++ tweet_me_screen/src/tweet_me_screen/__init__.py
@@ -0,0 +1,19 @@
+import web
+import os
+import roslib
+
+template_dir = roslib.packages.get_pkg_dir('tweet_me_screen') + '/templates'
+render = web.template.render(template_dir)
+
+def create_tweet_me_page(full_path, image_topic, headline, sayings):		
+	""" Generates the tweet me page at the given location. """
+	try:
+		os.makedirs(os.path.dirname(full_path))
+	except OSError, e:
+		pass
+
+	page = str(render.tweet_me(image_topic, headline, sayings))
+	with open(full_path, 'w+') as f:
+		f.write(page)
+
+	print('written ', full_path)
