da7fec738b8ad65aa9a620c85d25052af0656ba6
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2015-03-05 23:29:40 +0000

    Extended template to allow additional button to be templated.

--- tweet_me_screen/src/tweet_me_screen/__init__.py
+++ tweet_me_screen/src/tweet_me_screen/__init__.py
@@ -5,14 +5,14 @@ import roslib
 template_dir = roslib.packages.get_pkg_dir('tweet_me_screen') + '/templates'
 render = web.template.render(template_dir)
 
-def create_tweet_me_page(full_path, image_topic, headline, sayings):		
+def create_tweet_me_page(full_path, image_topic, headline, sayings, other_button=''):		
 	""" Generates the tweet me page at the given location. """
 	try:
 		os.makedirs(os.path.dirname(full_path))
 	except OSError, e:
 		pass
 
-	page = str(render.tweet_me(image_topic, headline, sayings))
+	page = str(render.tweet_me(image_topic, headline, sayings, other_button))
 	with open(full_path, 'w+') as f:
 		f.write(page)
 
