132f42903fa64083f94c50d3d2011b7264aee5c1
Jaime Pulido Fentanes <pulidofentanes@gmail.com>
2015-05-08 12:50:48 +0100

    Adding current task to the image

--- image_branding/scripts/image_superimposer.py
+++ image_branding/scripts/image_superimposer.py
@@ -5,6 +5,8 @@ import copy
 import rospy
 import cv2
 import numpy as np
+import rostopic
+
 
 from datetime import datetime
 from sensor_msgs.msg import Image
@@ -37,12 +39,20 @@ class SuperImposer(object):
         self.timeout_image = self.load_image_from_db(self.timeout_image_name) #cv2.imread(self.timeout_image_path)
         self.log_video=False
         self.closestNode='none'
+        self.scheduled_action= 'no schedule'
         rospy.loginfo(" ...starting")
 
         self.photo_pub = rospy.Publisher('/image_superimposer/output', Image, latch=True)
 
         
         self.logtim = rospy.get_rostime()
+        topics = rospy.get_published_topics()
+        topics = [x[0] for x in topics]
+        #print topics
+        if '/current_schedule' in topics:
+            toptyp = rostopic.get_topic_class('/current_schedule')
+            rospy.loginfo("Subscribing to /current_schedule")
+            rospy.Subscriber('/current_schedule', toptyp[0], self.scheduleCB)
         
         rospy.Subscriber('/closest_node', String, self.NodeCallback)
         rospy.Subscriber('/head_xtion/rgb/image_color', Image, self.ImCallback)
@@ -54,6 +64,16 @@ class SuperImposer(object):
  
         rospy.spin()
 
+
+    def scheduleCB(self, msg):
+        #print msg
+        if msg.currently_executing:
+            if len(msg.execution_queue)>0:
+                self.scheduled_action = msg.execution_queue[0].action + ' @ ' + msg.execution_queue[0].start_node_id
+            else:
+                self.scheduled_action = 'no task'
+        else:
+            self.scheduled_action = 'not executing'
 
     def ManagerCB(self, msg):
         self.log_video= msg.data
@@ -82,7 +102,7 @@ class SuperImposer(object):
         font = cv2.FONT_HERSHEY_SIMPLEX
 
 
-        #size1 = cv2.getTextSize(dt_text, font, 0.7, 2)
+        size1 = cv2.getTextSize(self.scheduled_action, font, 0.7, 2)
         size2 = cv2.getTextSize(ros_time_str, font, 0.7, 2)
         #print self.closestNode
         
@@ -90,12 +110,14 @@ class SuperImposer(object):
         hval = height-5
         hval2 = size2[0][1]+3
         wval2 = width - (size2[0][0] +3)
+        wval1 = width - (size1[0][0] +3)
         #bridge = CvBridge()
         
         
         cv2.putText(photo,dt_text,(3, hval), font, 0.7,(64,64,64),2)
-        cv2.putText(photo,ros_time_str,(wval2, hval2), font, 0.7,(64,64,64),2)
+        cv2.putText(photo,ros_time_str,(wval2, hval), font, 0.7,(64,64,64),2)
         cv2.putText(photo,self.closestNode,(3, hval2), font, 0.7,(64,64,64),2)
+        cv2.putText(photo,self.scheduled_action,(wval1, hval2), font, 0.7,(64,64,64),2)
         #width/2
         image_message = bridge.cv2_to_imgmsg(photo, encoding="bgr8")
         image_message.header = msg.header
@@ -114,12 +136,12 @@ class SuperImposer(object):
         message_list = msg_store.query(Image._type, {}, query_meta)
         
         if len(message_list) >0:
-            print message_list[0][1]
+            #print message_list[0][1]
             bridge = CvBridge()
             return bridge.imgmsg_to_cv2(message_list[0][0], "bgr8")
         else:
             width, height = 640, 480
-            red = (255, 0, 0)
+            #red = (255, 0, 0)
             image = create_blank(width, height, rgb_color=(255,255,255))
             return image
 
